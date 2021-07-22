130221f9f62ffdf3516d9c4f72c212dc116b33d4
Matthew E. Lowerr <matt@lowerr.com>
2021-07-21 20:03:46 -0500

    Formatting/linting.  Moved button onclick definitions to HTML file.  Added input validation to volume functions.

--- RokuController.js
+++ RokuController.js
@@ -15,9 +15,6 @@ const cbs = document.querySelectorAll('input[name="tv"]');
 
 // Functions associated with determining which TV's are in scope for operations
 
-tvToggleBtn = document.querySelector('#tvToggleBtn');
-tvToggleBtn.onclick = checkAll;
-
 function check(checked = true) {
   cbs.forEach((cb) => {
     cb.checked = checked;
@@ -26,12 +23,12 @@ function check(checked = true) {
 
 function checkAll() {
   check();
-  this.onclick = uncheckAll;
+  this.onclick = uncheckAll();
 }
 
 function uncheckAll() {
   check(false);
-  this.onclick = checkAll;
+  this.onclick = checkAll();
 }
 
 // Helper functions to send the post request(s) for all operations
@@ -54,120 +51,93 @@ function keyAction(reqAction) {
 // respond to this command.  The physical remote is needed to first turn the tv on.  This does work to turn a TV on that you
 // accidentilly turned them off.
 
-powerOnBtn.onclick = powerOn;
-
 function powerOn() {
   keyAction('PowerOn');
 }
-
-powerOffBtn.onclick = powerOff;
 
 function powerOff() {
   keyAction('PowerOff');
 }
 
-hdmi1Btn.onclick = setHDMI1Input;
-
 function setHDMI1Input() {
   keyAction('InputHDMI1');
 }
 
-homeBtn.onclick = setHomeInput;
-
 function setHomeInput() {
   keyAction('Home');
-}
-volumeUpBtn.onclick = volUp;
-
-function volUp() {
-  volLoop('VolumeUp');
-}
-
-volumeDownBtn.onclick = volDown;
-
-function volDown() {
-  volLoop('VolumeDown');
 }
 
 // This does not work with 100% accuracy.  There is a time between commands that the
 // TVs apparently need (no queing?), but it does help in quickly incrasing or decreasing
 // the volume.
 function volLoop(reqAction) {
-  increment = document.querySelector('input[name="volIncrement"]').value;
-  for (x = 0; x < increment; x++) {
+  let increment = document.querySelector('input[name="volIncrement"]').value;
+
+  // Guard against negative values and values greater than 20
+  if (increment < 0) {
+    increment = 1;
+  } else if (increment > 20) {
+    increment = 20;
+  }
+
+  for (let x = 0; x < increment; x += 1) {
     setTimeout(() => {
       keyAction(reqAction);
     }, 500);
   }
 }
 
-volumeMuteBtn.onclick = volMute;
+function volUp() {
+  volLoop('VolumeUp');
+}
+
+function volDown() {
+  volLoop('VolumeDown');
+}
 
 function volMute() {
   keyAction('VolumeMute');
 }
 
-backBtn.onclick = keyActionBack;
-
 function keyActionBack() {
   keyAction('Back');
 }
-
-upBtn.onclick = keyActionUp;
 
 function keyActionUp() {
   keyAction('Up');
 }
 
-downBtn.onclick = keyActionDown;
-
 function keyActionDown() {
   keyAction('Down');
 }
-
-leftBtn.onclick = keyActionLeft;
 
 function keyActionLeft() {
   keyAction('Left');
 }
 
-rightBtn.onclick = keyActionRight;
-
 function keyActionRight() {
   keyAction('Right');
 }
-
-selectBtn.onclick = keyActionSelect;
 
 function keyActionSelect() {
   keyAction('Select');
 }
 
-repeatBtn.onclick = keyActionRepeat;
-
 function keyActionRepeat() {
   keyAction('Repeat');
 }
-
-infoBtn.onclick = keyActionInfo;
 
 function keyActionInfo() {
   keyAction('Info');
 }
 
-revBtn.onclick = keyActionReverse;
-
 function keyActionReverse() {
   keyAction('Rev');
 }
 
-playBtn.onclick = keyActionPlay;
-
 function keyActionPlay() {
   keyAction('Play');
 }
-
-fwdBtn.onclick = keyActionForward;
 
 function keyActionForward() {
   keyAction('Fwd');
@@ -176,45 +146,57 @@ function keyActionForward() {
 // Keyboard shortcuts
 addEventListener('keyup', (event) => {
   switch (event.code) {
-    case 'ArrowDown':
+    case 'ArrowDown': {
       keyActionDown();
       break;
-    case 'ArrowUp':
+    }
+    case 'ArrowUp': {
       keyActionUp();
       break;
-    case 'ArrowLeft':
+    }
+    case 'ArrowLeft': {
       keyActionLeft();
       break;
-    case 'ArrowRight':
+    }
+    case 'ArrowRight': {
       keyActionRight();
       break;
-    case 'Enter':
+    }
+    case 'Enter': {
       keyActionSelect();
       break;
-    case 'KeyM':
+    }
+    case 'KeyM': {
       volMute();
       break;
-    case 'KeyJ':
+    }
+    case 'KeyJ': {
       volUp();
       break;
-    case 'KeyK':
+    }
+    case 'KeyK': {
       volDown();
       break;
-    case 'KeyH':
+    }
+    case 'KeyH': {
       setHomeInput();
       break;
-    case 'KeyC':
+    }
+    case 'KeyC': {
       setHDMI1Input();
       break;
+    }
     case 'Digit1':
     case 'Digit2':
     case 'Digit3':
     case 'Digit4':
-    case 'Digit5':
+    case 'Digit5': {
       const idx = event.code.substr(5) - 1;
       cbs[idx].click();
       break;
-    default:
+    }
+    default: {
       break;
+    }
   }
 });
